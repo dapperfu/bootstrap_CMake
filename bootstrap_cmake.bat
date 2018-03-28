9eb73307c12bfa2471a320ea6cd561e6e0161b3c
Jed <jed@m6700.local>
2018-03-28 01:48:35 -0700

    Added PROJECT_ENV tool

--- bootstrap_cmake.bat
+++ bootstrap_cmake.bat
@@ -1,4 +1,8 @@
 @ECHO OFF
+if "%PROJECT_ENV%"="" (
+set PROJECT_ENV=%~dp0
+)
+
 : If download URL is not defined (set through Jenkins, etc)
 IF "%CMAKE_URL%"=="" (
 : Use a known default
@@ -10,10 +14,12 @@ set URL=%CMAKE_URL%
 : Set download package name.
 set PKG=CMake.zip
 
-cd %~dp0
+cd %PROJECT_ENV%
 : If the package doesn't exist locally, download it.
-if not exist %PKG% %~dp0curl.exe -L -o %~dp0%PKG% %URL%
+if not exist %PKG% %~dp0curl.exe -L -o %PROJECT_ENV%%PKG% %URL%
 : If the package has not been 'installed' install it.
-if not exist %~dp0cmake-3.11.0-rc4-win64-x64 7z x %PKG% -y -o%~dp0
+if not exist %PROJECT_ENV%cmake-3.11.0-rc4-win64-x64 7z x %PKG% -y -o%PROJECT_ENV%
 
-set PATH=%PATH%;%~dp0cmake-3.11.0-rc4-win64-x64\bin
+set PATH=%PATH%;%PROJECT_ENV%cmake-3.11.0-rc4-win64-x64\bin
+
+pause
